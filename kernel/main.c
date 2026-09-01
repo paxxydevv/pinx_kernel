ecd97b664fec8ecdc5bbff8eb0aacfde7ae8ac1b
paxxydev <erene04780@gmail.com>
2026-09-01 15:46:17 +0300

    Adeded limine framebuffer request

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,24 @@
 #include "includes/pinx/gdt.h"
+#include "includes/3party/limine.h"
+__attribute__((used, section(".limine_requests_start")))
+static volatile uint64_t limine_requests_start_marker[] =
+    LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_base_revision[] =
+    LIMINE_BASE_REVISION(6);
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+    .revision = 0,
+    .response = 0
+};
+__attribute__((used, section(".limine_requests_end")))
+static volatile uint64_t limine_requests_end_marker[] =
+    LIMINE_REQUESTS_END_MARKER;
+
+
 void kmain() {
     gdt_init();
     for (;;) {
