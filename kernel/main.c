c998ff6a0ab262228e4a2e56e995e1d6b68d4ded
paxxydev <erene04780@gmail.com>
2026-09-01 15:56:03 +0300

    a

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,7 @@
 #include "includes/pinx/gdt.h"
+#include "includes/pinx/idt.h"
 #include "includes/3party/limine.h"
+
 __attribute__((used, section(".limine_requests_start")))
 static volatile uint64_t limine_requests_start_marker[] =
     LIMINE_REQUESTS_START_MARKER;
@@ -14,14 +16,23 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
     .revision = 0,
     .response = 0
 };
+
 __attribute__((used, section(".limine_requests_end")))
 static volatile uint64_t limine_requests_end_marker[] =
     LIMINE_REQUESTS_END_MARKER;
 
-
-void kmain() {
+void kmain(void)
+{
     gdt_init();
+    idt_init();
+    if (framebuffer_request.response == 0 ||
+        framebuffer_request.response->framebuffer_count < 1) {
+        for (;;) {
+            __asm__ volatile ("hlt");
+        }
+    }
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
     for (;;) {
-        __asm__ volatile("hlt");
+        __asm__ volatile ("hlt");
     }
 }
